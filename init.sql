4e537f39d9de4d8ba9696d98d5ac92980b11a74f
Zerazzt <2003isaaclee@gmail.com>
2023-04-03 11:32:11 -0400

    Add login table and procedures for certain win rate statistics.

--- init.sql
+++ init.sql
@@ -44,6 +44,15 @@ CREATE TABLE `game_tags` (
 	FOREIGN KEY (id) REFERENCES games(id)
 );
 
+CREATE TABLE `admin` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`email` varchar(64) NOT NULL,
+	`password` text NOT NULL,
+	`token` text DEFAULT NULL,
+	`token_expiry` bigint(20) DEFAULT NULL,
+	PRIMARY KEY (id)
+);
+
 DELIMITER $$
 CREATE TRIGGER `game_added` BEFORE INSERT ON `game_participation`
 FOR EACH ROW
@@ -94,3 +103,205 @@ BEGIN
 END;
 $$
 DELIMITER ;
+
+CREATE PROCEDURE `RestrictedDeckWinRate`(IN `player_count` INT)
+NOT DETERMINISTIC
+CONTAINS SQL
+SQL SECURITY DEFINER
+SELECT
+	rgp.deck_id,
+	rgp.commander,
+	COALESCE(deck_wins.wins, 0) AS `wins`,
+	rgp.count - COALESCE(deck_wins.wins, 0) AS `losses`,
+	100 * COALESCE(deck_wins.wins, 0) / rgp.count AS `win rate`
+FROM (
+	SELECT decks.id AS `id`,
+		decks.commander,
+		COUNT(rgames.winning_deck) AS `wins`
+	FROM decks
+	RIGHT JOIN (
+		SELECT
+			games.winning_deck
+		FROM games
+		RIGHT JOIN game_participation
+		ON games.id = game_participation.game_id
+		GROUP BY games.id
+		HAVING COUNT(games.id) = player_count
+	) AS `rgames`
+	ON decks.id = rgames.winning_deck
+	GROUP BY decks.id
+) AS `deck_wins`
+RIGHT JOIN (
+	SELECT
+		game_participation.deck_id,
+		decks.commander,
+		COUNT(game_participation.deck_id) AS `count`
+	FROM game_participation
+	RIGHT JOIN (
+		SELECT
+			games.id AS `id`
+		FROM games
+		RIGHT JOIN game_participation
+		ON game_participation.game_id = games.id
+		GROUP BY games.id
+		HAVING COUNT(games.id) = player_count
+	) AS `allrgames`
+	ON game_participation.game_id = allrgames.id
+	LEFT JOIN decks
+	ON game_participation.deck_id = decks.id
+	GROUP BY deck_id
+) AS `rgp`
+ON deck_wins.id = rgp.deck_id
+ORDER BY `win rate` DESC;
+
+CREATE PROCEDURE `OverallDeckWinRate`()
+NOT DETERMINISTIC
+CONTAINS SQL
+SQL SECURITY DEFINER
+SELECT
+	rgp.deck_id,
+	rgp.commander,
+	COALESCE(deck_wins.wins, 0) AS `wins`,
+	rgp.count - COALESCE(deck_wins.wins, 0) AS `losses`,
+	100 * COALESCE(deck_wins.wins, 0) / rgp.count AS `win rate`
+FROM (
+	SELECT
+		decks.id AS `id`,
+		decks.commander,
+		COUNT(rgames.winning_deck) AS `wins`
+	FROM decks
+	RIGHT JOIN (
+		SELECT
+			games.winning_deck
+		FROM games
+		RIGHT JOIN game_participation
+		ON games.id = game_participation.game_id
+		GROUP BY games.id
+		HAVING COUNT(games.id) > 0
+	) AS `rgames`
+	ON decks.id = rgames.winning_deck
+	GROUP BY decks.id
+) AS `deck_wins`
+RIGHT JOIN (
+	SELECT
+		game_participation.deck_id,
+		decks.commander,
+		COUNT(game_participation.deck_id) AS `count`
+	FROM game_participation
+	RIGHT JOIN (
+		SELECT
+			games.id AS `id`
+		FROM games
+		RIGHT JOIN game_participation
+		ON game_participation.game_id = games.id
+		GROUP BY games.id
+		HAVING COUNT(games.id) > 0 
+	) AS `allrgames`
+	ON game_participation.game_id = allrgames.id
+	LEFT JOIN decks
+	ON game_participation.deck_id = decks.id
+	GROUP BY deck_id
+) AS `rgp`
+ON deck_wins.id = rgp.deck_id
+ORDER BY `win rate` DESC;
+
+CREATE PROCEDURE `RestrictedPlayerWinRate`(IN `player_count` INT)
+NOT DETERMINISTIC
+CONTAINS SQL
+SQL SECURITY DEFINER
+SELECT
+	rgp.player_id,
+	rgp.name,
+	COALESCE(player_wins.wins, 0) AS `wins`,
+	rgp.count - COALESCE(player_wins.wins, 0) AS `losses`,
+	100 * COALESCE(player_wins.wins, 0) / rgp.count AS `win rate`
+FROM (
+	SELECT
+		players.id AS `id`,
+		players.name,
+		COUNT(rgames.winning_player) AS `wins`
+	FROM players
+	RIGHT JOIN (
+		SELECT
+			games.winning_player
+		FROM games
+		RIGHT JOIN game_participation
+		ON games.id = game_participation.game_id
+		GROUP BY games.id
+		HAVING COUNT(games.id) = player_count
+	) AS `rgames`
+	ON players.id = rgames.winning_player
+	GROUP BY players.id
+) AS `player_wins`
+RIGHT JOIN (
+	SELECT
+		game_participation.player_id, players.name,
+		COUNT(game_participation.player_id) AS `count`
+	FROM game_participation
+	RIGHT JOIN (
+		SELECT
+			games.id AS `id`
+		FROM games
+		RIGHT JOIN game_participation
+		ON game_participation.game_id = games.id
+		GROUP BY games.id
+		HAVING COUNT(games.id) = player_count
+	) AS `allrgames`
+	ON game_participation.game_id = allrgames.id
+	LEFT JOIN players
+	ON game_participation.player_id = players.id
+	GROUP BY player_id
+) AS `rgp`
+ON player_wins.id = rgp.player_id
+ORDER BY `win rate` DESC;
+
+CREATE PROCEDURE `OverallPlayerWinRate`()
+NOT DETERMINISTIC
+CONTAINS SQL
+SQL SECURITY DEFINER
+SELECT
+	rgp.player_id,
+	rgp.name,
+	COALESCE(player_wins.wins, 0) AS `wins`,
+	rgp.count - COALESCE(player_wins.wins, 0) AS `losses`,
+	100 * COALESCE(player_wins.wins, 0) / rgp.count AS `win rate`
+FROM (
+	SELECT
+		players.id AS `id`,
+		players.name,
+		COUNT(rgames.winning_player) AS `wins`
+	FROM players
+	RIGHT JOIN (
+		SELECT
+			games.winning_player
+		FROM games
+		RIGHT JOIN game_participation
+		ON games.id = game_participation.game_id
+		GROUP BY games.id
+		HAVING COUNT(games.id) > 0
+	) AS `rgames`
+	ON players.id = rgames.winning_player
+	GROUP BY players.id
+) AS `player_wins`
+RIGHT JOIN (
+	SELECT
+		game_participation.player_id,
+		players.name,
+	COUNT(game_participation.player_id) AS `count`
+	FROM game_participation
+	RIGHT JOIN (
+		SELECT
+			games.id AS `id`
+		FROM games
+		RIGHT JOIN game_participation
+		ON game_participation.game_id = games.id
+		GROUP BY games.id
+		HAVING COUNT(games.id) > 0
+	) AS `allrgames`
+	ON game_participation.game_id = allrgames.id
+	LEFT JOIN players
+	ON game_participation.player_id = players.id
+	GROUP BY player_id
+) AS `rgp`
+ON player_wins.id = rgp.player_id
+ORDER BY `win rate` DESC;
