3a8fa87712fe9080a0cb46644be47e6c6f4deb40
Zerazzt <2003isaaclee@gmail.com>
2024-02-01 21:59:50 -0500

    Fix page title display issue.

--- php/includes/head.php
+++ php/includes/head.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title><?= $page_titles[$page_name] ?></title>
+	<title><?= $pages[$page_name]['title'] ?></title>
 	<link rel="stylesheet" href="/css/main.css">
 	<script src = "https://kit.fontawesome.com/38a787eaeb.js"></script>
 </head>
