7014b78a0ce369cef1c620a9016181ff89d50b42
Zerazzt <isaaclee@isaaclee.ca>
2024-10-05 16:51:23 -0400

    Add Cloudflare tracking tools.

--- php/includes/head.php
+++ php/includes/head.php
@@ -8,5 +8,8 @@
 	<title><?= $pages[$page_name]['title'] ?></title>
 	<link rel="stylesheet" href="/css/main.css">
 	<script src = "https://kit.fontawesome.com/38a787eaeb.js"></script>
+	<!-- Cloudflare Web Analytics -->
+	 <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "721030c655174fdb9a58988363817702"}'></script>
+	 <!-- End Cloudflare Web Analytics -->
 </head>
 <body>
